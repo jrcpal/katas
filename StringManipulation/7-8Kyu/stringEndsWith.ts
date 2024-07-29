c07161109b8b02252a48b248e687c11d09717ce9
Jessica Churgin <Jessica.Churgin@vertigis.com>
2024-07-29 13:20:53 -0400

    change folder structure

---
+++ StringManipulation/7-8Kyu/stringEndsWith.ts
@@ -0,0 +1,43 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+// Solution
+// ------------------------------------------
+
+function solution(str: string, ending: string) {
+    return str.endsWith(ending)
+}
+
+// Console
+// ------------------------------------------
+
+console.log(solution('abcde', 'de'))
+
+// Notes
+// ------------------------------------------
+
+// .endsWith method came out with ES6 in 2015
+// Alternative solution 
+
+function solution2(str: string, ending: string) {
+    return str.slice(0, -ending.length)
+}
+
+// slice method extracts a section of a string or an array and returns it as a new string or array without modifying the original
+// slice syntax is string(or array).slice(beginIndex, endIndex) , with endIndex being optional
+
+
+// Original Test
+// ------------------------------------------
+
+// describe("Tests", () => {
+//     it("test", () => {
+//   Test.assertEquals(solution('abcde', 'cde'), true)
+//   Test.assertEquals(solution('abcde', 'abc'), false)
+//   });
+//   });
+  
