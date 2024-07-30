e6a0d7631065ac77a235fe568c9f06b5f8092394
Jessica Churgin <jchurgy@gmail.com>
2024-07-30 13:58:10 -0400

    disemvowel and highestAndLowest

---
+++ NumberManipulation/7-8kyu/highestAndLowest.ts
@@ -0,0 +1,47 @@
+// Instructions
+// ------------------------------------------
+//
+//In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+// Examples
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+// Notes
+// All numbers are valid Int32, no need to validate them.
+// There will always be at least one number in the input string.
+// Output string must be two numbers separated by a single space, and highest number is first.
+
+// Solution
+// ------------------------------------------
+
+function highAndLow(numbers: string){
+    let arr = numbers.split(' ').map(Number);
+    return `${Math.max(...arr)} ${Math.min(...arr)}`
+}
+
+// Console
+// ------------------------------------------
+console.log("Function highAndLow expects '3 -1' and returned: ", highAndLow("-1 0 1 2 3"))
+
+
+
+// Notes
+// ------------------------------------------
+// Had to add .map(Number) to convert string type from original argument to number for typescript
+
+// alternative solution, but less effective 
+function highAndLow2(numbers: string) {
+    let arr = numbers.split(' ').map(Number).sort((a,b) => Number(b)-Number(a))
+    return `${arr[0]} ${arr[arr.length-1]}`
+}
+
+
+// Original Test
+// ------------------------------------------
+
+// it("Test 1", () => {
+//     assert.strictEqual(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"), "42 -9");
+//   });
+//   it("Test 2", () => {
+//     assert.strictEqual(highAndLow("1 2 3"), "3 1");
---
+++ StringManipulation/7-8Kyu/disemvowel.ts
@@ -0,0 +1,44 @@
+// Instructions
+// ------------------------------------------
+//
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+// Note: for this kata y isn't considered a vowel.
+
+// Solution
+// ------------------------------------------
+
+function disemvowel(str: string) {
+    return str.replace(/[aeiou]/gi, '')
+}
+
+// Console
+// ------------------------------------------
+
+console.log("The function disemvowel expects 'Hw nw brwn cw?' and returns: ", disemvowel("How now brown cow?"))
+
+
+// Notes
+// ------------------------------------------
+//
+// replace method takes in a search value, and a new value
+// .replace(searchValue, newValue)
+// the / / denotes a regular expression
+// the [ ] creates a character set 
+// the g makes it global, instead of just the first occurance
+// the i makes it case Insensitive. 
+
+// Alternative solution
+
+function disemvowel2(str: string) {
+    const vowels = 'aeiouAEIOU'
+    return str.split('').filter(char => !vowels.includes(char)).join('')
+}
+
+
+
+// Original Test
+// ------------------------------------------
+// assert.strictEqual(disemvowel("This website is for losers LOL!"), "Ths wbst s fr lsrs LL!")
+// assert.strictEqual(disemvowel("No offense but,\nYour writing is among the worst I've ever read"), "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd")
+// assert.strictEqual(disemvowel("What are you, a communist?"), "Wht r y,  cmmnst?")
